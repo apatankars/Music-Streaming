0218911d6e1bc485094653f4811c1670b696b51f
apatankars <armaan_patankar@brown.edu>
2023-12-22 19:04:20 -0500

    Implemented up to player

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: [
+            "ylcbbzppmjdfbvfouvqp.supabase.co"
+        ]
+    }
+}
+
+module.exports = nextConfig
